b861868ab022c14a39d8c2b905d592796501fb4a
Cmoyaa <81742799+Cmoyaa@users.noreply.github.com>
2021-12-23 01:49:07 -0300

    arreglo transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,13 @@ const paneles = document.querySelectorAll('.panel');
 
 paneles.forEach(panel => {
     panel.addEventListener('click', () => {
+        removeActiveClasses();
         panel.classList.add('activo');
     })
 })
+
+function removeActiveClasses() {
+    paneles.forEach(panel => {
+        panel.classList.remove('activo')
+    })
+}
